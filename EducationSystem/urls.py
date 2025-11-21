e7e96b3588bebccd8ffdbd7a6daaeeba6ac51ec0
Dorsa Khalili <147814777+dorrsakh@users.noreply.github.com>
2025-11-21 16:19:19 +0330

    create base layout for the Education System. Added welcome, login, dashboard, and profile pages.

--- EducationSystem/urls.py
+++ EducationSystem/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from . import views
 
-app_name = 'ConfigurationChangeRequest'
+# app_name = 'ConfigurationChangeRequest'
 
 urlpatterns = [
     # ایجاد درخواست جدید
-    path('', views.test, name='request_create'),
+    # path('', views.test, name='request_create'),
+    path('', views.welcome, name='welcome'),           # صفحه اول
+    path('login/', views.login_view, name='login'),    # صفحه لاگین
+    path('dashboard/', views.dashboard_view, name='dashboard'),  # صفحه داشبورد
+    path('profile/', views.profile_view, name='profile'),        # صفحه پروفایل
     ]
+    
--- EducationSystem/views.py
+++ EducationSystem/views.py
@@ -3,3 +3,25 @@ from django.shortcuts import render
 # Create your views here.
 def test(request):
     pass
+
+def welcome(request):
+    return render(request, "EducationSystem/pages/welcome.html")
+
+def login_view(request):
+    if request.method == "POST":
+        # اینجا بعداً می‌تونی لاجیک ورود را اضافه کنی
+        student_id = request.POST.get("student_id")
+        password = request.POST.get("password")
+
+        # TODO: اعتبارسنجی – بعداً اضافه می‌کنیم
+        pass
+
+    return render(request, "EducationSystem/pages/login.html")
+
+# صفحه داشبورد
+def dashboard_view(request):
+    return render(request, "EducationSystem/pages/dashboard.html")
+
+# صفحه پروفایل
+def profile_view(request):
+    return render(request, "EducationSystem/pages/profile.html")
